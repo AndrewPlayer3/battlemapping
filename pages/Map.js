2f2bff2f5c92bcc3273e6709135b24f0868fe16b
Andrew Player <andrewplayer3@gmail.com>
2022-08-30 01:57:21 -0500

    broken  -- switching off of next

--- pages/Map.js
+++ pages/Map.js
@@ -1,28 +1,38 @@
-import { useState, useEffect } from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import { useState, useEffect } from 'react'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, useMap, GeoJSON } from 'react-leaflet'
+import ReactLeafletKml from 'react-leaflet-kml';
+import Ukraine from './Ukraine.js'
 
 export function ChangeView({ coords }) {
-  const map = useMap();
-  map.setView(coords, 12);
-  return null;
+    const map = useMap()
+    map.setView(coords, 12)
+    return null
 }
 
-export default function Map() {
-  const [geoData, setGeoData] = useState({ lat: 64.536634, lng: 16.779852 });
+  
 
-  const center = [geoData.lat, geoData.lng];
+export default function Map({kml}) {
+    
+    const [geoData, setGeoData] = useState({ lat: 64.536634, lng: 16.779852 })
 
-  return (
-    <MapContainer center={center} zoom={12} style={{ height: '100vh' }}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {geoData.lat && geoData.lng && (
-        <Marker position={[geoData.lat, geoData.lng]} />
-      )}
-      <ChangeView coords={center} />
-    </MapContainer>
-  );
+    const center = [geoData.lat, geoData.lng]
+
+    return (
+        <MapContainer center={center} zoom={12} style={{ height: '100vh' }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+            {geoData.lat && geoData.lng && (
+                <Marker position={[geoData.lat, geoData.lng]} />
+            )}
+
+            <GeoJSON data={Ukraine.features }/>
+            {
+                console.log(kml)
+            }
+            <ChangeView coords={center} />
+        </MapContainer>
+    )
 }
