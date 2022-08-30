c0bc469c646c0e0a390e1a0961a0435db8e85262
Andrew Player <andrewplayer3@gmail.com>
2022-08-30 17:24:57 -0500

    Basic Layer Selection

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
 import ReactLeafletKml from 'react-leaflet-kml';
 
 function Map(props) {
-    const [kml, setKml] = React.useState(null);
+    const [positionsKml, setPositionsKml] = React.useState(null);
+    const [trenchesKml,  setTrenchesKml]  = React.useState(null);
     React.useEffect(() => {
         fetch(
         "russian_positions.kml"
@@ -11,17 +12,33 @@ function Map(props) {
         .then((res) => res.text())
         .then((kmlText) => {
             const parser = new DOMParser();
-            const kml = parser.parseFromString(kmlText, "text/xml");
-            setKml(kml);
+            const positionsKml = parser.parseFromString(kmlText, "text/xml");
+            setPositionsKml(positionsKml);
+        });
+        fetch(
+            "ukraine_trenches.kml"
+            )
+            .then((res) => res.text())
+            .then((kmlText) => {
+                const parser = new DOMParser();
+                const trenchesKml = parser.parseFromString(kmlText, "text/xml");
+                setTrenchesKml(trenchesKml);
         });
     }, []);
     return (
         <MapContainer center={[48.443786, 31.792798]} zoom={6}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
-            {kml && <ReactLeafletKml kml={kml} />}
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />  
+                <LayersControl position="topright">
+                    <LayersControl.Overlay checked  name="Positions">
+                        {positionsKml && <ReactLeafletKml kml={positionsKml} />}
+                    </LayersControl.Overlay>
+                    <LayersControl.Overlay name="Trenches">
+                        {trenchesKml && <ReactLeafletKml kml={trenchesKml} />}
+                    </LayersControl.Overlay>
+                </LayersControl>
         </MapContainer>
     )
 };
