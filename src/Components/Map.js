4250435470776070037d4400942813f88057cbc2
Andrew Player <andrewplayer3@gmail.com>
2022-08-30 03:08:37 -0500

    cleaned + changed to russian positions file

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,22 +1,12 @@
 import * as React from "react";
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import L from 'leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import ReactLeafletKml from 'react-leaflet-kml';
-import massGraveRed from '../assets/Mass-Grave-Marker-Red.png';
-
-function GetMarkerIcon(_iconSize) {
-    return new L.Icon({
-        iconUrl: massGraveRed,
-        iconSize: [31, 45],
-        iconAnchor: [15, 45]
-    });
-};
 
 function Map(props) {
     const [kml, setKml] = React.useState(null);
     React.useEffect(() => {
         fetch(
-        "Ukraine.kml"
+        "russian_positions.kml"
         )
         .then((res) => res.text())
         .then((kmlText) => {
@@ -26,16 +16,12 @@ function Map(props) {
         });
     }, []);
     return (
-        <MapContainer center={[23.4, -102.5]} zoom={6}>
+        <MapContainer center={[48.443786, 31.792798]} zoom={6}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
             {kml && <ReactLeafletKml kml={kml} />}
-            <Marker 
-            position={ [ 31.165863, -110.959835 ] }
-            icon = { GetMarkerIcon() }
-            ></Marker>
         </MapContainer>
     )
 };
